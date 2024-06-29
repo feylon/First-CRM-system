e6b48c17992229782f3b46b304ee79d6123e32f6
feylon <jamshid14092002@gmail.com>
2024-06-29 17:07:41 +0500

    get_caterogies tayyor bo'ldi

--- backend/Routers/admin/get_product_types.js
+++ backend/Routers/admin/get_product_types.js
@@ -1,2 +1,35 @@
 import { Router } from "express";
 import Joi from "joi";
+
+const router = Router();
+
+router.get("/", async function (req, res){
+
+    const Schema = Joi.object(
+        {
+            page : Joi.number().required().min(1),
+            size : Joi.number().required().min(1)
+        }
+    );
+    const checkSchema = Schema.validate(req.query);
+    if(checkSchema.error) return res.status(400).send(checkSchema.error.message)
+try {
+
+    const {page, size} = req.query;
+        const data = await global.pool.query(
+    `
+    
+SELECT * FROM categories
+LIMIT ${size} OFFSET (${page} - 1)  *  ${size};	
+    `
+        )
+
+return res.status(200).send(data.rows)
+} catch (error) {
+    res.status(500).send("Serverda xatolik : (");
+    console.log("! Error : get_caterogies: ", error)
+}
+
+});
+
+export default router;
--- backend/Routers/admin/index.js
+++ backend/Routers/admin/index.js
@@ -1,7 +1,9 @@
 import login from "./login.js";
 import add_caterogies from "./add_caterogies.js"
+import get_caterogies from "./get_product_types.js"
 export default 
 [
 [login,"login"],
-[add_caterogies,"add_caterogies"]
+[add_caterogies,"add_caterogies"],
+[get_caterogies, "get_caterogies"]
 ];
