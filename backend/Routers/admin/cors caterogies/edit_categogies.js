4711c8f4d33709169e80bf99b347fe2d9e9b8ded
feylon <jamshid14092002@gmail.com>
2024-06-29 19:13:15 +0500

    caterogies tugadi to'liq

---
+++ backend/Routers/admin/cors caterogies/edit_categogies.js
@@ -0,0 +1,43 @@
+import Joi from "joi";
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/:id", async function (req, res){
+
+    const Scheme = Joi.object({
+        name : Joi.string().min(3).max(50).trim().required(),
+        state : Joi.boolean()
+    });
+    const checkSchema = Scheme.validate(req.body);
+
+    if(checkSchema.error) return res.status(400).send(checkSchema.error.message);
+    // if((typeof (req.params.id) )!= 'number' )
+        // return res.status(400).send(":id Butun son bo'lishi lozim")
+    const {name, state} = req.body;
+
+
+    try {
+        let check_content = 
+        await global.pool.query(
+            "Select id from categories where id = $1 ",
+            [req.params.id]
+        );
+        if(!check_content.rows.length)
+            return res.status(404).send("Ma'lumot mavjud emas");
+        
+        let update_content = await global.pool.query
+        ("Update categories set name = $1, state = $2 where id = $3",
+          [name,state, req.params.id]  
+
+        );
+        return res.status(201).send("Edited")
+    } catch (error) {
+        if(error.code == "23505")
+            return res.status(400).send(error.detail)
+        console.log(error)
+    }
+});
+
+
+export default router;
--- backend/Routers/admin/index.js
+++ backend/Routers/admin/index.js
@@ -1,11 +1,13 @@
 import login from "./login.js";
-import add_caterogies from "./add_caterogies.js"
-import get_caterogies from "./get_categogies.js"
-import edit_caterogies from "./edit_categogies.js"
+import add_caterogies from "./cors caterogies/add_caterogies.js";
+import get_caterogies from "./cors caterogies/get_categogies.js";
+import edit_caterogies from "./cors caterogies/edit_categogies.js";
+import delete_caterogies from "./cors caterogies/delete_categories.js"
 export default 
 [
 [login,"login"],
 [add_caterogies,"add_caterogies"],
 [get_caterogies, "get_caterogies"],
-[edit_caterogies,"edit_caterogies"]
+[edit_caterogies,"edit_caterogies"],
+[delete_caterogies,"delete_caterogies"]
 ];
