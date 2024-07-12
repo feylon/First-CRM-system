b2f74b3603700a9c3f42583d4f0de2c703db7d5e
feylon <jamshid14092002@gmail.com>
2024-07-12 15:50:06 +0500

    saqlash tugatildi

--- backend/Functions/jwt.js
+++ backend/Functions/jwt.js
@@ -1,22 +1,34 @@
 import jwt from "jsonwebtoken";
+import  dotenv  from "dotenv";
+dotenv.config();
+function sign(id) {
+  return jwt.sign({ id }, process.env.tokenAdminCode, { expiresIn: "1h" });
+}
 
-function sign(id){
-   return jwt.sign({id},"Yashirin",{expiresIn:"1h"})
-};
+function token_check(req, res, next) {
+  try {
+    ;
+    let token = req.header("-x-token");
+    let decoded = jwt.verify(token, process.env.tokenAdminCode);
+    req.header("-x-token",eval(decoded.id))
+    
+    next();
+  } catch (err) {
+    return res.status(401).send("Token eskirgan");
+  }
+}
 
-function token_check(id){
-    try{
-        let token = req.header("-x-token")    
-        let decoded = jwt.verify(token,"YashirinKOD");
+function get_id(req, res, next){
+    try {
+        let token = req.header("-x-token");
+        let decoded = jwt.verify(token, "YashirinKOD");
         // console.log(decoded);
-        req.body.jwt_id = decoded.id;
+    
+        // req.body.jwt_id = decoded.id;
+    
         next();
-        }
-        catch(err){
+      } catch (err) {
         return res.status(400).send("Token eskirgan");
-        }
-        
-        
-        
-}   
-export {sign, token_check}
+      } 
+}
+export { sign, token_check };
--- backend/Routers/admin/tasks/create.js
+++ backend/Routers/admin/tasks/create.js
@@ -2,7 +2,11 @@ import multer from "multer";
 import md5 from "md5";
 import Joi from "joi";
 import  {Router} from "express";
-import fs from "fs"
+import fs from "fs";
+import { token_check } from "../../../Functions/jwt.js";
+import  dotenv  from "dotenv";
+import jwt from "jsonwebtoken"
+dotenv.config();
 (async()=>{
     try {
         await global.pool.query(`
@@ -23,7 +27,8 @@ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 active boolean default true,
 rate integer default 0,
 MaxRate integer,	
-done boolean default false 
+done boolean default false ,
+attempt integer default 1
  );`);
 
                 console.log("Task database yaratildi");
@@ -45,7 +50,7 @@ let storage = multer.diskStorage({
       let filename = `${name}.${ext}`;
       cb(null, filename);
       req.body.task_file = { url: filename, name: file.originalname };
-      console.log("Bajarildi");
+      
   }
 });
 
@@ -53,25 +58,23 @@ let upload = multer({
   limits: { fileSize:  1024 * 1024 },
       fileFilter: (req, file, cb) => {
         cb(null, true);
-        console.log(file)
-        
+             
   },
   storage: storage
 }).single("file");
 const router = Router();
 
-router.post("/",upload, function(req, res,){
-  console.log(req.body)
+router.post("/",[token_check,upload], async function(req, res,){
   
 const body = {
 name : req.body.name ,
 description : req.body.description,
-admin_id : req.body.admin_id,
 worker_id : req.body.worker_id,
 diedline : req.body.diedline,
-active : req.body.active,
+active : req.body.active == 'on' ? true : false  ,
 MaxRate : req.body.MaxRate,
-done : req.body.done,
+done : req.body.done == 'on' ? true : false,
+attempt : req.body.attempt
 }
   
 
@@ -79,21 +82,51 @@ done : req.body.done,
   const Schema = Joi.object({
     name : Joi.string().min(5).max(500).required().trim(),
     description : Joi.string().min(10).max(500).required().trim(),
-    admin_id : Joi.number().min(0).required(),
     worker_id : Joi.number().min(0).required(),
-    diedline : Joi.date().required(),
+    diedline : Joi.string().required(),
     active : Joi.boolean().required(),
     MaxRate : Joi.number().min(0).required(),
+    attempt : Joi.number().min(0).required().max(10),
     done :  Joi.boolean().required()
 });
+body
 const checkSchema = Schema.validate(body);
 if(checkSchema.error) {
   fs.unlink(`${process.cwd()}/static/task/${req.body.task_file.url}`,(err)=>{
     if(err) return console.log(err) 
       }) ;
-  return res.status(400).send(checkSchema.error.message)}
+      console.log(checkSchema.error.message);
+      console.log("BODY : ", req.body);
+      return res.status(400).send(checkSchema.error.message)
+}
+     
+     const {id} = jwt.verify(req.header("-x-token"), process.env.tokenAdminCode);;
+try {
+  await global.pool.query(
+    `
+    insert into task
+(name, description, task_file, task_file_name, admin_id, worker_id, diedline, active, maxrate, done, attempt)
+values
+($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
+`,
+    [body.name, body.description, req.body.task_file.name, req.body.task_file.url, id, body.worker_id, body.diedline, body.active, body.MaxRate, body.done,  body.attempt]  
+  )
+} catch (error) {
+  if(error.code == "23503"){
+    fs.unlink(`${process.cwd()}/static/task/${req.body.task_file.url}`,(err)=>{
+      if(err) return console.log(err) 
+      }) ;
+    return res.status(400).send(error.detail)
+  };
+  if(error.code == "22008") {
+    fs.unlink(`${process.cwd()}/static/task/${req.body.task_file.url}`,(err)=>{
+      if(err) return console.log(err) 
+      }) ;
+    return res.status(400).send("Sana vaqt xato")
+  };
+  console.log(error)
 
-  res.send(req.body)
+}
   
   
   
